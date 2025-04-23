c4f90444951fbb4a09594606301b9cb8a397b28e
Chaitanya <chaitu.vsh@gmail.com>
2025-04-22 22:43:00 -0700

    README.md

--- src/main/kotlin/designpatterns/creational/factory/TransportationFactory.kt
+++ src/main/kotlin/designpatterns/creational/factory/TransportationFactory.kt
@@ -1,6 +1,8 @@
 package system.design.designpatterns.creational.factory
 
 class TransportationFactory {
+
+    // This is a parameterised factory method.
     fun provideTransportation(transportationMode: TransportationMode): Transportation {
         return when(transportationMode) {
             TransportationMode.ROAD -> RoadTransportation()
